bf0af8ac3dd98d4eeb0e9914ff7f5ca2db914df7
Somesh Meshram <93580770+somesh-mesh@users.noreply.github.com>
2024-05-21 16:43:36 +0530

    fetch request to show notification count

--- 7.4_recoil_deep_drive/recoil_deep_drive/src/App.jsx
+++ 7.4_recoil_deep_drive/recoil_deep_drive/src/App.jsx
@@ -1,43 +1,37 @@
-import { useMemo, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil'
-import { jobAtom, messagingAtom, networkAtom, notificationAtom, totalNotificationSelector } from './atoms'
+import { useEffect } from 'react';
+import './App.css';
+import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil';
+import { notificationsFromApi, totalNotificationSelector } from './atoms';
 
 function App() {
   return (
     <RecoilRoot>
       <MainApp />
     </RecoilRoot>
-  )
+  );
 }
-
 
 function MainApp() {
-
-  const networkNotificationCount = useRecoilValue(networkAtom);
-  const jobCount = useRecoilValue(jobAtom);
-  const messagingCount = useRecoilValue(messagingAtom);
-  const networkAtomCount = useRecoilValue(notificationAtom); 
+  const [notifications, setNotifications] = useRecoilState(notificationsFromApi);
   const totalNotificationCount = useRecoilValue(totalNotificationSelector);
 
-// const totalNotificationCount = useMemo(()=>{
-//   return networkNotificationCount+jobCount+messagingCount+networkAtomCount;
-// },[networkNotificationCount,jobCount,messagingCount,networkAtomCount]);
-
-  const [messagingAtomCount,setMessagingCount] = useRecoilState(messagingAtom);
-  return <>
-  <button>Home</button>
-    <button>My Network {networkNotificationCount >= 100 ? "99+" : networkNotificationCount}</button>
-    <button>Jobs({jobCount})</button>
-    <button>Messaging({messagingCount})</button>
-    <button>Notification({networkAtomCount})</button>
-    <button onClick={()=>{
-      setMessagingCount(messagingAtomCount +1);
-    }}>Me ({totalNotificationCount})</button>
-
-  </>
+  return (
+    <>
+      <button>Home</button>
+      <button>My Network {notifications.network >= 100 ? "99+" : notifications.network}</button>
+      <button>Jobs ({notifications.jobs})</button>
+      <button>Messaging ({notifications.messaging})</button>
+      <button>Notification ({notifications.notifications})</button>
+      <button onClick={() => {
+        setNotifications(prevState => ({
+          ...prevState,
+          messaging: prevState.messaging + 1
+        }));
+      }}>
+        Me ({totalNotificationCount})
+      </button>
+    </>
+  );
 }
 
-export default App
+export default App;
--- 7.4_recoil_deep_drive/recoil_deep_drive/src/atoms.js
+++ 7.4_recoil_deep_drive/recoil_deep_drive/src/atoms.js
@@ -1,37 +1,28 @@
+import axios from "axios";
 import { atom, selector } from "recoil";
 
-export const networkAtom = atom({
-  key: "networkAtom",
-  default: 102,
+// Atom to store notification data from the API
+export const notificationsFromApi = atom({
+  key: "notificationsFromApi",
+  default: selector({
+    key: "networkAtomSelector",
+    get: async () => {
+      await new Promise(r => setTimeout(r, 500)); // Simulate delay
+      const res = await axios.get("https://sum-server.100xdevs.com/notifications");
+      return res.data;
+    }
+  }),
 });
 
-export const jobAtom = atom({
-  key: "jobAtom",
-  default: 0,
-});
-
-export const notificationAtom = atom({
-  key: "notificationAtom",
-  default: 12,
-});
-
-export const messagingAtom = atom({
-  key: "messagingAtom",
-  default: 0,
-});
-
+// Selector to compute the total number of notifications
 export const totalNotificationSelector = selector({
   key: "totalNotificationSelector",
   get: ({ get }) => {
-    const networkAtomCount = get(networkAtom);
-    const jobAtomCount = get(jobAtom);
-    const notificationAtomCount = get(notificationAtom);
-    const messagingAtomCount = get(messagingAtom);
-    return (
-      networkAtomCount +
-      jobAtomCount +
-      notificationAtomCount +
-      messagingAtomCount
-    );
+    const notifications = get(notificationsFromApi);
+    const networkCount = notifications.network;
+    const jobCount = notifications.jobs;
+    const messagingCount = notifications.messaging;
+    const notificationCount = notifications.notifications;
+    return networkCount + jobCount + messagingCount + notificationCount;
   },
 });
