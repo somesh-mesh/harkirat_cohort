d8d7ca4c902c96cb03e2271dba597bc9f80517d8
Somesh Meshram <93580770+somesh-mesh@users.noreply.github.com>
2024-05-27 01:06:37 +0530

    created custom hooks for data fetching

--- week 9.1/hooks-prac/src/App.jsx
+++ week 9.1/hooks-prac/src/App.jsx
@@ -1,88 +1,38 @@
-import React, { useState, useEffect } from 'react';
-// import React from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
-
-const [render,setRender] = useState(true);
-
-    useEffect(()=>{
-      setInterval(()=>{
-        setRender( r=> !r);
-      },5000)
-    },[]);
-
-
+  const todos = useTodos();
   return (
     <>
-      {render ? <MyComponent/> : <div></div>}      
+      {todos.map(todo => <Track todo={todo} />)}
     </>
   )
 }
 
-// function MyComponent() {
-//   const [count, setCount] = useState(0);
+function useTodos() {
 
-//   const incrementCount = () => {
-//     setCount(count + 1);
-//   }
+  const [todos, setTodos] = useState([]);
 
-//   return (
-//     <>
-//       <div>
-//         <p>{count}</p>
-//         <button onClick={incrementCount}>Increament</button>
-//       </div>
-//     </>
-//   )
-
-
-
-// }
-
-
-// class MyComponent extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { count: 0 };
-//   }
-
-//   incrementCount = () => {
-//     this.setState({ count: this.state.count + 1 });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <p>{this.state.count}</p>
-//         <button onClick={this.incrementCount}>Increment</button>
-//       </div>
-//     )
-//   }
-
-function MyComponent() {
   useEffect(() => {
+    axios.get("https://sum-server.100xdevs.com/todos")
+      .then(res => {
+        setTodos(res.data.todos);
+      })
+  }, [])
 
-    console.error("Component: mounted");
-
-    return () => {
-      // Cleanup code (similar to componentWillUnmount)
-      console.error("Component: unmounted");
-    };
-  }, []);
-
-  // Render UI
-
-  return <div>
-    From inside my compoenent
-  </div>
+  return todos;
 
 }
 
-// }
 
 
-
-
+function Track({ todo }) {
+  return <div>
+    {todo.title}
+    <br />
+    {todo.description}
+  </div>
+}
 
 export default App
