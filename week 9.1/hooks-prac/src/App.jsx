4b6f9bb2707f35c3814ca94cd009e3aa11a1e8c9
Somesh Meshram <93580770+somesh-mesh@users.noreply.github.com>
2024-05-27 01:14:30 +0530

    second variation

--- week 9.1/hooks-prac/src/App.jsx
+++ week 9.1/hooks-prac/src/App.jsx
@@ -2,37 +2,51 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function App() {
-  const todos = useTodos();
+  // Corrected destructuring syntax and adjusted return value from useTodos
+  const [todos, loading] = useTodos();
+
+  if (loading) {
+    return <div>loading.....</div>
+  }
+
   return (
     <>
-      {todos.map(todo => <Track todo={todo} />)}
+      {todos.map((todo, index) => (
+        // Added key prop to each Track component
+        <Track key={index} todo={todo} />
+      ))}
     </>
   )
 }
 
 function useTodos() {
-
   const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios.get("https://sum-server.100xdevs.com/todos")
       .then(res => {
         setTodos(res.data.todos);
+        setLoading(false);
       })
-  }, [])
+      .catch(error => {
+        console.error("Error fetching todos:", error);
+        setLoading(false);
+      });
+  }, []);
 
-  return todos;
-
+  // Corrected return to use array syntax
+  return [todos, loading];
 }
-
-
 
 function Track({ todo }) {
-  return <div>
-    {todo.title}
-    <br />
-    {todo.description}
-  </div>
+  return (
+    <div>
+      {todo.title}
+      <br />
+      {todo.description}
+    </div>
+  )
 }
 
-export default App
+export default App;
