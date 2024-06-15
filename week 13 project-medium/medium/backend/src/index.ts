306db6b9b5e0b9605d29fba00da72659bb50a4c8
Somesh Meshram <93580770+somesh-mesh@users.noreply.github.com>
2024-06-16 00:28:53 +0530

    routes add

--- week 13 project-medium/medium/backend/src/index.ts
+++ week 13 project-medium/medium/backend/src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
-import { sign } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import userRouter from "./user";
+import { blogRouter } from "./blog";
+
 
 const app = new Hono<{
   Bindings: {
@@ -10,75 +13,7 @@ const app = new Hono<{
   };
 }>();
 
-app.post("/api/v1/signup", async (c) => {
-  console.log("DATABASE_URL:", c.env?.DATABASE_URL);
-  console.log("JWT_SECRET:", c.env?.JWT_SECRET);
-
-  const prisma = new PrismaClient({
-    datasources: {
-      db: {
-        url: c.env?.DATABASE_URL,
-      },
-    },
-  }).$extends(withAccelerate());
-
-  const body = await c.req.json();
-  if (!body.email || !body.password) {
-    c.status(400);
-    return c.json({ error: "email and password are required" });
-  }
-
-  try {
-    const user = await prisma.user.create({
-      data: {
-        email: body.email,
-        password: body.password,
-      },
-    });
-    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-    return c.json({ jwt });
-  } catch (e) {
-    console.error("Error during signup:", e);
-    c.status(403);
-    return c.json({ error: "error while signing up" });
-  }
-});
-
-app.post('/api/v1/signin', async (c) => {
-  const prisma = new PrismaClient({
-    datasources: {
-      db: {
-        url: c.env?.DATABASE_URL,
-      },
-    },
-  }).$extends(withAccelerate());
-
-  const body = await c.req.json();
-  const user = await prisma.user.findUnique({
-    where: {
-      email: body.email,
-    },
-  });
-
-  if (!user) {
-    c.status(403);
-    return c.json({ error: "user not found" });
-  }
-
-  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-  return c.json({ jwt });
-});
-
-app.post("/api/v1/blog", (c) => {
-  return c.text("blog route");
-});
-
-app.get("/api/v1/blog/:id", (c) => {
-  return c.text("blog id route");
-});
-
-app.get("/api/v1/blog/bulk", (c) => {
-  return c.text("blog bulk route");
-});
+app.route('/api/v1/user', userRouter);
+app.route('/api/v1/blog', blogRouter);
 
 export default app;
